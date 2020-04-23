77eafaea095ffb0fe4904d2d8d24a16749231cd6
Alan Cornejo <alancornejo@icloud.com>
2020-04-22 21:39:31 -0500

    Cambiando mensaje

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hola Mundo, desde mi maquina"
+    return "Hola Mundo, mi nombre es Pachaqtec"
 
 if __name__ == "__main__":
     app.run(port=80, debug=False)
