956a4f255f88e34a7f5b814bbc74dd3e2cd05f8f
Alan Cornejo <alancornejo@icloud.com>
2020-04-22 21:42:09 -0500

    Agregado variables de entorno

--- application.py
+++ application.py
@@ -1,9 +1,12 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
+nombre = os.environ['nombre']
+
 @app.route('/')
 def index():
-    return "Hola Mundo, mi nombre es Pachaqtec"
+    return f"Hola Mundo, mi nombre es : {nombre}...!!!!"
 
 if __name__ == "__main__":
     app.run(port=80, debug=False)
